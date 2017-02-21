95a00a478ee4694b8dcfe5ba54f29944d9c9e419
bsanford <sanford.braeden@gmail.com>
2017-02-20 22:18:19 -0500

    Added status file parsing

--- pid_analysis.c
+++ pid_analysis.c
@@ -14,6 +14,8 @@ int parse_cmd(analysis_struct_t *process, const char *buffer);
 static int process_cmdline(analysis_struct_t *process, char *buffer, size_t buf_size);
 static int process_mstat(analysis_struct_t *process, char *buffer, size_t buf_size);
 static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_size);
+void print_analysis(analysis_struct_t *processes, size_t buf_size);
+static int  process_status(analysis_struct_t *process, char *buffer, size_t buf_size);
 
 
 
@@ -30,13 +32,14 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
  * Once the pids are initializes the structures will be packed.
  * @return
  */
-    int main(void) {
+    int main(void){
 
         char *mpid_file;
         pid_t pid_max;
         analysis_struct_t *processes;
 
-        asprintf(&mpid_file, "%s%s", PROC, MAX_PID_LOC); //TODO check pid file
+       if((asprintf(&mpid_file, "%s%s", PROC, MAX_PID_LOC)) == -1)
+            exit(EXIT_FAILURE);
 
         if ((pid_max = get_maxpids(mpid_file)) == -1) {
             fprintf(stderr, "Error retrieving maximum pids on the system \n");
@@ -75,7 +78,7 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
         DIR *proc_dir;
         struct dirent *pid_dir;
         pid_t pidval;
-        char *pid_path , buffer[BUF_SIZE];
+        char buffer[BUF_SIZE];
 
         if ((proc_dir = opendir(PROC)) == NULL) {
             perror("Open Dir:");
@@ -94,7 +97,10 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
             }
 
             processes[pidval].pid = pidval;
-            asprintf(&(processes[pidval].path), "%s/%s", PROC, pid_dir->d_name);
+            if((asprintf(&(processes[pidval].path), "%s/%s", PROC, pid_dir->d_name)) == -1){
+                fprintf(stderr, "Error couldn't generate proc path \n");
+                return (-1);
+            }
 
             if((process_cmdline(&processes[pidval], buffer, BUF_SIZE)) == -1){ //Process cmdline proc file
                 fprintf(stderr, "Error in cmdline process \n");
@@ -111,27 +117,45 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
                 return (-1);
             }
 
-            free(pid_path); //free the memory allocated from the pid path
+            if((process_status(&processes[pidval], buffer, BUF_SIZE))== -1){ //Process stat proce file
+                fprintf(stderr, "Error in stat process \n");
+                return (-1);
+            }
+
+
         }
 
+         print_analysis(processes, max_pids);
         return (0);
 
     }
 
 
 
+
+
+
+
 /**Function process_mstat
  *
- * @param process
- * @param buffer
- * @param buf_size
- * @return
+ * @brief processes and parses the mstat file, puts all data elements of mstat into mstat structure, then maps
+ *        to the analysis_t structure. Design wise would be better to just parse mstat into a structure and then have a
+ *        seperate mapping function so it could be more re usable but since this was academic I lumped it all together.
+ *
+ * @param process - the current pid being processed
+ * @param buffer - buffer to hold contents of mstat
+ * @param buf_size - size of the buffer
+ * @return 0 on success -1 on failure
  */
 
 static int process_mstat(analysis_struct_t *process, char *buffer, size_t buf_size){
     char *cmd_path;
+    int page_size;
 
-    asprintf(&cmd_path, "%s/%s", process->path, "statm");
+    if((asprintf(&cmd_path, "%s/%s", process->path, "statm")) == -1){
+        fprintf(stderr, "Couldn't generate mstat path");
+        return (-1);
+    }
 
     statm_struct_t *mstat = malloc(sizeof(status_struct_t));
 
@@ -142,6 +166,9 @@ static int process_mstat(analysis_struct_t *process, char *buffer, size_t buf_si
         return (-1);
     }
 
+    /*Each value in mstat is the count of pages, so page size in bytes * number of pages = total number of bytes*/
+    page_size = getpagesize();
+
 
     if ((sscanf(buffer,"%llu%llu%llu%llu%llu%llu%llu",&(mstat->size), &(mstat->resident), &(mstat->share),
                                                       &(mstat->text),  &(mstat->lib),
@@ -151,11 +178,53 @@ static int process_mstat(analysis_struct_t *process, char *buffer, size_t buf_si
         free(cmd_path);
         return (-1);
     }
+    process->res_set_size = mstat->resident * page_size;
+    process->txt_mem_size = mstat->share * page_size;
+    process->lib_mem_size = mstat->lib * page_size;
+    process->shared_mem_size = mstat->share * page_size;
 
     free(mstat);
     free(cmd_path);
      return(0);
 }
+
+
+
+
+
+
+
+
+/**Function print_analysis
+ *
+ * Loops over the array of pointers to processes and printfs CSV formatted output of packed data
+ *
+ * @param processes the start address to the array of analysis_t pointers
+ * @param buf_size how many pointers there are
+ */
+
+void print_analysis(analysis_struct_t *processes, size_t buf_size){
+
+    size_t index;
+    printf("PID,PPID,PGID,START_TIME,CPU_USE_TIME,CPU_SYS_TIME,RES_SET_SIZE,TXT_MEM_SIZE, DATA_MEM_SIZE,"
+                   "LIB_MEM_SIZE,MAJOR_FAULTS,MINOR_FAULTS,REAL_USR_ID,EFF_USR_ID,REAL_GRP_ID,EFCTV_GROUP_ID,"
+                   "CMD \n");
+    for(index = 1; index <= buf_size; index++) {
+        if (processes[index].pid == 0)
+            continue;
+
+        printf("%d,%d,%d,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%ld,%d,%d,%d,%d,%s \n",
+               processes[index].pid, processes[index].ppid, processes[index].pgid,
+               processes[index].start_time.tv_sec, processes[index].cpu_use_time.tv_sec,
+               processes[index].cpu_sys_time.tv_sec,processes[index].res_set_size,processes[index].txt_mem_size,
+               processes[index].data_mem_size,processes[index].lib_mem_size, processes[index].major_faults,
+               processes[index].minor_faults,processes[index].real_usr_id,processes[index].eff_usr_id,
+               processes[index].real_grp_id,processes[index].efctv_grp_id,processes[index].cmd);
+    }
+
+
+}
+
 
 
 
@@ -177,7 +246,11 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
      char *strt;
      char *end;
     status_struct_t *stat = malloc(sizeof(status_struct_t));
-     asprintf(&cmd_path, "%s/%s", process->path, "stat");
+
+    if((asprintf(&cmd_path, "%s/%s", process->path, "stat")) == -1){
+        fprintf(stderr, "Couldn't generate stat path \n");
+        return (-1);
+    }
     
     if ((get_buffer(cmd_path, buffer, buf_size)) == -1) {
         fprintf(stderr, "Couldn't define the cmd_line parameter \n");
@@ -229,14 +302,80 @@ static int process_stat(analysis_struct_t *process, char *buffer, size_t buf_siz
 
 
 
-/**Function process status
+
+static int get_status_line(const char *process_path, char *buffer, size_t buf_size, const char *srch){
+
+
+    FILE *file;
+    memset(buffer, '\0', buf_size);
+
+    if ((file = fopen(process_path, "r")) == NULL) {
+        perror("Error opening cmdfile :");
+        return (-1);
+    }
+
+
+    while (fgets(buffer, buf_size, file) != NULL) {
+        if ((strstr(buffer, srch)) != NULL) {
+            fclose(file);
+            return (0);
+        }
+    }
+  fclose(file);
+
+   fprintf(stderr, "File does not contain search string \n");
+  return (-1);
+
+}
+
+
+
+
+
+
+
+
+/**Process_Staus
+ * @brief process status goes through the status file looking specifically for
+ *        UID and GID data. When the given status field is found in the file it
+ *        then parses the fields for the required GID and UID data
  *
- * Processes the status file to get the UUIDs of the process
- *
+ * @param process
+ * @param buffer
+ * @param buf_size
+ * @return -1 on error 0 on success
  */
 
-void process_status(void){
+static int  process_status(analysis_struct_t *process, char *buffer, size_t buf_size){
+    char *cmd_path;
 
+    if((asprintf(&cmd_path, "%s/%s", process->path, "status")) == -1){
+        fprintf(stderr, "Couldn't generate cmd line path \n");
+        return (-1);
+    }
+
+    if((get_status_line(cmd_path, buffer, buf_size, "Uid")) == -1){
+        fprintf(stderr, "Uid not found in STATUS file erroring \n");
+        return (-1);
+    }
+
+    if((sscanf(buffer, "Uid: %d %d", &(process->real_usr_id), &(process->eff_usr_id))) == -1){
+        fprintf(stderr , "Couldn't parse UID buffer \n");
+        return (-1);
+    }
+
+
+    if((get_status_line(cmd_path, buffer, buf_size, "Gid")) == -1){
+        fprintf(stderr, "Gid not found in STATUS file erroring \n");
+        return (-1);
+    }
+
+    if((sscanf(buffer, "Gid: %d %d", &(process->real_grp_id), &(process->efctv_grp_id))) == -1){
+        fprintf(stderr , "Couldn't parse GID buffer \n");
+        return (-1);
+    }
+
+    return(0);
 }
 
 
@@ -262,19 +401,26 @@ static int process_cmdline(analysis_struct_t *process, char *buffer, size_t buf_
 
     char *cmd_path;
 
-    asprintf(&cmd_path, "%s/%s", process->path, "cmdline"); //TODO add check
+    if((asprintf(&cmd_path, "%s/%s", process->path, "cmdline")) == -1){
+        fprintf(stderr, "Couldn't generate cmd line path \n");
+        return (-1);
+    }
 
     if ((get_buffer(cmd_path, buffer, buf_size)) == -1) {
         fprintf(stderr, "Couldn't define the cmd_line parameter \n");
+        free(cmd_path);
         return (1);
     }
 
     if((parse_cmd(process, buffer)) == -1){
         fprintf(stderr, "Couldn't parse the command line file \n");
+        free(cmd_path);
         return (-1);
     }
     return (0);
 }
+
+
 
 
 
@@ -315,9 +461,9 @@ static int process_cmdline(analysis_struct_t *process, char *buffer, size_t buf_
 
 
 
-
-
 /**Function get_cmd
+ * @brief prepares the buffer for use, since this is a syncronis process there is only one char buffer that is
+ *        shared between all the parsed files under /proc
  *
  * @param process - pointer to the struct that maps to the process id
  * @param process_path process path - path to the process that we are working with
@@ -336,6 +482,7 @@ static int process_cmdline(analysis_struct_t *process, char *buffer, size_t buf_
 
         fgets(buffer, buf_size, file);
 
+
         if ((fclose(file)) == EOF) {
             perror("Error closing command line file :");
             return (-1);
@@ -351,7 +498,12 @@ static int process_cmdline(analysis_struct_t *process, char *buffer, size_t buf_
 
 
 
-
+/**Function parse_cmd
+ *
+ * @param process
+ * @param buffer
+ * @return
+ */
 int parse_cmd(analysis_struct_t *process, const char *buffer){
     int lnth;
 
